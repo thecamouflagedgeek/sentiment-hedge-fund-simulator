596e53d629a309342d44a02539f076ffdac218b0
thecamouflagedgeek <pretentiousgazette@gmail.com>
2025-12-15 20:01:20 +0530

    Update database connection string for production environment

--- backend/db/db_connect.py
+++ backend/db/db_connect.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:sirius*@localhost:5432/sentiment_db"
+DATABASE_URL = "postgresql+psycopg2://postgresql://aura_db_l0o9_user:46aAh2aKai98VZiwsSlUOgfH5gJgvlEI@dpg-d501maemcj7s73e177d0-a.virginia-postgres.render.com/aura_db_l0o9"
 
 engine=create_engine(DATABASE_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush = False,bind=engine)
